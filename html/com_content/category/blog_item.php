b6b5cdf2fc46cbbfef51fbbd864d7753328332a0
CJ Melegrito <cj@mlgrto.com>
2015-12-02 21:03:16 +1100

    added comments for legibility

--- html/com_content/category/blog_item.php
+++ html/com_content/category/blog_item.php
@@ -10,17 +10,21 @@
 
 // render page if authorised, else don't
 defined('_JEXEC') or die;
+
+// abbreviate Joomla's post object
 $post = $this->item;
 
 ?>
 
 <article id="<?php echo $post->id; ?>">
+  <?php // render the post's title and link ?>
   <h1>
     <a href="<?php echo $post->link; ?>">
       <?php echo $post->title; ?>
     </a>
   </h1>
 
+  <?php // render the post's metadata ?>
   <ul>
     <li><?php echo $post->publish_up; ?></li>
     <li><?php echo $post->author; ?></li>
@@ -28,15 +32,19 @@ $post = $this->item;
   </ul>
 
   <?php
+    // decode post object's images JSON content
     $images = json_decode($post->images);
     $image_intro = htmlspecialchars($images->image_intro);
     $image_intro_alt = htmlspecialchars($images->image_intro_alt);
   ?>
 
+  <?php // render the post's intro image ?>
   <img src="<?php echo $image_intro; ?>" alt="<?php echo $image_intro_alt; ?>"/>
 
+  <?php // render the post's intro text ?>
   <?php echo $post->introtext; ?>
 
+  <?php // render the post's full text (after the intro) ?>
   <?php echo $post->fulltext; ?>
 
 </article>
